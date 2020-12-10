b8061ecc4eabedc22f639a3c39664bd932f5247f
CRico <cesarrico16@gmail.com>
2020-12-09 23:33:27 -0500

    Agregue Reporte Pedidos totales

--- ServidorJAVA-ClienteCSharp/ProyectosNET/CrewmanSystem/frmReportePedidosXCliente.Designer.cs
+++ ServidorJAVA-ClienteCSharp/ProyectosNET/CrewmanSystem/frmReportePedidosXCliente.Designer.cs
@@ -32,6 +32,8 @@
             this.btnGenerar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.cboTipoCliente = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnGenerar
@@ -40,9 +42,10 @@
             this.btnGenerar.FlatAppearance.BorderSize = 0;
             this.btnGenerar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnGenerar.ForeColor = System.Drawing.Color.White;
-            this.btnGenerar.Location = new System.Drawing.Point(320, 210);
+            this.btnGenerar.Location = new System.Drawing.Point(240, 105);
+            this.btnGenerar.Margin = new System.Windows.Forms.Padding(2);
             this.btnGenerar.Name = "btnGenerar";
-            this.btnGenerar.Size = new System.Drawing.Size(160, 30);
+            this.btnGenerar.Size = new System.Drawing.Size(120, 24);
             this.btnGenerar.TabIndex = 8;
             this.btnGenerar.Text = "Generar";
             this.btnGenerar.UseVisualStyleBackColor = false;
@@ -51,9 +54,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(110, 95);
+            this.label1.Location = new System.Drawing.Point(82, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.Size = new System.Drawing.Size(84, 13);
             this.label1.TabIndex = 9;
             this.label1.Text = "Tipo de Cliente: ";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -62,19 +66,49 @@
             // 
             this.cboTipoCliente.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboTipoCliente.FormattingEnabled = true;
-            this.cboTipoCliente.Location = new System.Drawing.Point(266, 95);
+            this.cboTipoCliente.Location = new System.Drawing.Point(200, 38);
+            this.cboTipoCliente.Margin = new System.Windows.Forms.Padding(2);
             this.cboTipoCliente.Name = "cboTipoCliente";
-            this.cboTipoCliente.Size = new System.Drawing.Size(121, 24);
+            this.cboTipoCliente.Size = new System.Drawing.Size(92, 21);
             this.cboTipoCliente.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(219, 250);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 25);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Generar Reporte Pedidos";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 205);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Reporte total de pedidos:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // frmReportePedidosXCliente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.cboTipoCliente);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnGenerar);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmReportePedidosXCliente";
             this.Text = "frmReportePedidosXCliente";
             this.ResumeLayout(false);
@@ -88,5 +122,7 @@
         private System.Windows.Forms.Button btnGenerar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cboTipoCliente;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
     }
 }
