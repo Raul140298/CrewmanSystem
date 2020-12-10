e6b941de6c7eed5517d027868fdc03ca9511d393
a20170587@outlook.com <a20170587@outlook.com>
2020-12-10 10:47:01 -0500

    c

--- ServidorJAVA-ClienteCSharp/ServidorJAVA/CrewmanSystemServices/src/java/pe/edu/pucp/CrewmanSystem/servlets/ReportePedidosXCliente.java
+++ ServidorJAVA-ClienteCSharp/ServidorJAVA/CrewmanSystemServices/src/java/pe/edu/pucp/CrewmanSystem/servlets/ReportePedidosXCliente.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import javax.servlet.ServletException;
